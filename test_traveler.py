17b8cab802f871b1895af14d9abc6d872f426771
AlexMirallas <command.shockwave1@gmail.com>
2024-03-25 17:20:40 +0100

    Added logic to find traveler destinations and convert internal data

--- test_traveler.py
+++ test_traveler.py
@@ -1,2 +1,29 @@
-destinations = ["USA", "UK", "Sweden","Norway","Germany","Greece","Turkey","Japan"]
-test_traveler = "Alex"
+destinations = ["Paris, France", "Shangai, China","Los Angeles, USA", "Sao Paolo, Brazil","Cairo Egypt"]
+
+test_traveler = ["Erin Wilkes", "Shangai, China",["historical site","art"]]
+
+def get_destination_index(destination):
+    destination_index = destinations.index(destination)
+    return destination_index
+    
+
+
+#print(get_destination_index("Paris, France"))
+
+def get_traveler_location(traveler):
+    traveler_destination = traveler[1]
+    traveler_destination_index = traveler.index(traveler_destination)
+    print(traveler_destination_index)
+
+
+get_traveler_location(test_traveler)
+
+
+
+    
+
+
+
+
+
+
